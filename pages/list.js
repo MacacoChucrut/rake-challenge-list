1985d68bd3d30e7f3bb94d9707bcf8d5b9623379
MacacoChucrut <93385434+MacacoChucrut@users.noreply.github.com>
2026-09-08 10:39:04 -0300

    Update list.js

--- pages/list.js
+++ pages/list.js
@@ -1,6 +1,6 @@
 import { getThumbnailFromId, getLevelPoints } from "../utils.js";
 
-export async function renderList(content, listName = "demonlist") {
+export async function renderList(content, listName = "list") {
     content.innerHTML = `
     <div class="list-page">
         <div id="list"></div>
