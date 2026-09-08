71fac3eca1122ed087c3fac9ddb8024ee8458af4
MacacoChucrut <93385434+MacacoChucrut@users.noreply.github.com>
2026-09-08 10:37:24 -0300

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ import { renderStatsViewer } from "./pages/stats_viewer.js";
 
 const statsViewerBtn = document.getElementById("nav-stats-viewer");
 const content = document.getElementById("content");
-const DEFAULT_LIST = "demonlist";
+const DEFAULT_LIST = "list";
 
 function router() {
     const path = location.hash.replace(/^#\/?/, ""); 
