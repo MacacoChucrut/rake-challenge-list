cc13e7d4c9df62ee363e89a0822c4b6cc136aeac
MacacoChucrut <93385434+MacacoChucrut@users.noreply.github.com>
2026-09-08 13:03:57 -0300

    Update level.js

--- pages/level.js
+++ pages/level.js
@@ -19,8 +19,9 @@ export async function renderLevel(content, listName = "demonlist", id) {
                 <div class="level-main">
                     <div class="level-title">
                         <h1>${level.name}</h1>
-                        <p>by ${level.creator}</p>
-                        <p>published by ${level.publisher}</p>
+                        <p>By ${level.creator}</p>
+                        <p>Published by ${level.publisher}</p>
+                        <p>Verified by ${level.verifier}</p>
                     </div>
 
                     <div class="level-media">
